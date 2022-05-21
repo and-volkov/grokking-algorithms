fc0d908570470ed58314ca0993ad5a13272086cb
Андрей Волков <andrey.dreamwall@gmail.com>
2022-05-21 21:37:39 +0300

    recursion

---
+++ recursion/recursion.py
@@ -0,0 +1,27 @@
+def sum_arr(arr):
+    """Сумма всех элементов списка"""
+    if not arr:
+        return 0
+    else:
+        return arr[0] + sum_arr(arr[1:])
+
+
+def arr_count(arr):
+    """Подсчет элементов списка"""
+    counter = 0
+    if not arr:
+        return 0
+    else:
+        return (counter + 1) + arr_count(arr[1:])
+
+
+def arr_max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = max(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+
+arr1 = [6, 2, 3, 4, 5]
+
+print(arr_max(arr1))
